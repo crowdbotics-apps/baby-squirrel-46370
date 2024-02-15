3c42fbe1e0ab4c931777e83c52cbb272a8d5bd6d
Crowdbotics <team@crowdbotics.com>
2024-02-15 07:25:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "baby-squirrel-46370",
+  url: "https://baby-squirrel-46370.botics.co",
+  api: "https://baby-squirrel-46370.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
